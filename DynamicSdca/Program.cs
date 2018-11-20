2a427608434fb1500bd7e1eead47a8accdf50fb7
Monte Hoover <mohoov@microsoft.com>
2018-11-20 13:49:53 -0800

    Static, Dynamic, and Legacy API's all working.

--- DynamicSdca/Program.cs
+++ DynamicSdca/Program.cs
@@ -37,21 +37,22 @@ namespace DynamicSdca
                 // Default separator is tab, but the dataset has comma.
                 Separator = ","
             });
+            var data = reader.Read(dataPath);
 
+            // Build the training pipeline.
             var pipeline =
                 // Concatenate all the features together into one column 'Features'.
-                new ConcatEstimator(env, "Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
                 // Note that the label is text, so it needs to be converted to key.
-                .Append(new ToKeyEstimator(env, "Label"))
+                .Append(mlContext.Transforms.Categorical.MapValueToKey("Label"), TransformerScope.TrainTest)
                 // Use the multi-class SDCA model to predict the label using features.
                 .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent())
                 // Apply the inverse conversion from 'PredictedLabel' column back to string value.
                 .Append(mlContext.Transforms.Conversion.MapKeyToValue(("PredictedLabel", "Data")));
 
-            var data = reader.Read(new MultiFileSource(dataPath));
-            var model = pipeline.Fit(data).AsDynamic;
+            var model = pipeline.Fit(data);
 
-            var predictionEngine = model.MakePredictionFunction<IrisInput, IrisPrediction>(env);
+            var predictionEngine = model.MakePredictionFunction<IrisInput, IrisPrediction>(mlContext);
             var prediction = predictionEngine.Predict(new IrisInput
             {
                 SepalLength = 4.1f,
@@ -59,67 +60,6 @@ namespace DynamicSdca
                 PetalLength = 3.2f,
                 PetalWidth = 1.4f
             });
-
-            //// Apply all kinds of standard ML.NET normalization to the raw features.
-            //var normalizerPipeline = reader.MakeNewEstimator()
-            //    .Append(r => (
-            //        MinMaxNormalized: r.Features.Normalize(fixZero: true),
-            //        MeanVarNormalized: r.Features.NormalizeByMeanVar(fixZero: false),
-            //        CdfNormalized: r.Features.NormalizeByCumulativeDistribution(),
-            //        BinNormalized: r.Features.NormalizeByBinning(maxBins: 256)
-            //    ));
-
-            //// Let's train our pipeline of normalizers, and then apply it to the same data.
-            //var normalizedData = normalizerPipeline.Fit(data).Transform(data);
-
-            //// Inspect one column of the resulting dataset.
-            //var minMaxValues = normalizedData.GetColumn(r => r.MinMaxNormalized).ToArray();
-            //var meanVarValues = normalizedData.GetColumn(r => r.MeanVarNormalized).ToArray();
-            //var cdfValues = normalizedData.GetColumn(r => r.CdfNormalized).ToArray();
-            //var binValues = normalizedData.GetColumn(r => r.BinNormalized).ToArray();
-
-
-            // STEP 3: Transform your data
-
-            // Assign numeric values to text in the "Label" column, because only
-            // numbers can be processed during model training
-
-            // Legacy API
-            //pipeline.Add(new Dictionarizer("Label"));
-
-            // Puts all features into a vector
-
-            // Legacy API
-            //pipeline.Add(new ColumnConcatenator("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"));
-
-            // STEP 4: Add learner
-            // Add a learning algorithm to the pipeline. 
-            // This is a classification scenario (What type of iris is this?)
-
-            // Legacy API
-            //pipeline.Add(new StochasticDualCoordinateAscentClassifier());
-
-            // Convert the Label back into original text (after converting to number in step 3)
-
-            // Legacy API
-            //pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
-
-            // STEP 5: Train your model based on the data set
-
-            // Legacy API
-            //var model = pipeline.Train<IrisDataLegacy, IrisPredictionLegacy>();
-
-            // STEP 6: Use your model to make a prediction
-            // You can change these numbers to test different predictions
-
-            // Legacy API
-            //var prediction = model.Predict(new IrisDataLegacy()
-            //{
-            //    SepalLength = 3.3f,
-            //    SepalWidth = 1.6f,
-            //    PetalLength = 0.2f,
-            //    PetalWidth = 5.1f,
-            //});
 
             Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabel}");
         }
